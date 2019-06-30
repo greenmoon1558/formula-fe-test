1f3e17149ef4847bb48b497af493c1edc11d2ec7
Natalia Zhydejkina <nata.iiinx88@gmail.com>
2019-06-30 23:06:44 +0300

    add netlify.toml

---
+++ Level1_Html_css/html-landing/src/js/index.js
@@ -0,0 +1,8 @@
+import 'lazysizes';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(reg => console.log('serviceWorker is registered', reg))
+    .catch(err => console.log('serviceWorker is not registered. ', err));
+}
